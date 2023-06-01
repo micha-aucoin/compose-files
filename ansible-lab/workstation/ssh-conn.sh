607b8e9557e9a50d93807331f4cea4134e66d110
micha-aucoin <maucoin94@gmail.com>
2023-06-01 13:26:30 -0500

    init

---
+++ ansible-lab/workstation/ssh-conn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Generate sysbox_workstation SSH key
+echo "Generating sysbox_workstation SSH key"
+ssh-keygen -t ed25519 -f /home/admin/.ssh/sysbox_workstation -C "sysbox_workstation" 
+
+# Copy sysbox_workstation SSH key to target hosts
+ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node1
+ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node2
+ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node3
+
+# Generate ansible SSH key
+echo "Generating ansible SSH key"
+ssh-keygen -t ed25519 -f /home/admin/.ssh/ansible -C "ansible" 
+
+# Copy ansible SSH key to target hosts
+ssh-copy-id -i /home/admin/.ssh/ansible.pub admin@node1
+ssh-copy-id -i /home/admin/.ssh/ansible.pub admin@node2
+ssh-copy-id -i /home/admin/.ssh/ansible.pub admin@node3
