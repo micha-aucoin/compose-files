76d57195d84a29c7adde69c2fd166fe37ff28df9
micha-aucoin <maucoin94@gmail.com>
2023-06-03 18:46:38 -0500

    added python-dev-env

--- ansible-lab/workstation/ssh-conn.sh
+++ ansible-lab/workstation/ssh-conn.sh
@@ -5,9 +5,9 @@ echo "Generating sysbox_workstation SSH key"
 ssh-keygen -t ed25519 -f /home/admin/.ssh/workstation -C "sysbox_workstation" 
 
 # Copy sysbox_workstation SSH key to target hosts
-ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node1
-ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node2
-ssh-copy-id -i /home/admin/.ssh/sysbox_workstation.pub admin@node3
+ssh-copy-id -i /home/admin/.ssh/workstation.pub admin@node1
+ssh-copy-id -i /home/admin/.ssh/workstation.pub admin@node2
+ssh-copy-id -i /home/admin/.ssh/workstation.pub admin@node3
 
 # Generate ansible SSH key
 echo "Generating ansible SSH key"
